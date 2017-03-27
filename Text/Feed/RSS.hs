aa6886327a87aac64677d3b9e65000599b402cc3
mrkkrp <markkarpov@opmbx.org>
2017-03-28 01:00:00 +0300

    Add all the missing languages for RSS

--- Text/Feed/RSS.hs
+++ Text/Feed/RSS.hs
@@ -13,6 +13,7 @@
 --
 -- > import qualified Text.Feed.RSS
 
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 
@@ -103,10 +104,92 @@ data Language
   | ChineseTraditional
   | Croatian
   | Czech
-
+  | Danish
+  | Dutch
+  | DutchBelgium
+  | DutchNetherlands
   | English
+  | EnglishAustralia
+  | EnglishBelize
+  | EnglishCanada
+  | EnglishIreland
+  | EnglishJamaica
+  | EnglishNewZealand
+  | EnglishPhillipines
+  | EnglishSouthAfrica
+  | EnglishTrinidad
+  | EnglishUnitedKingdom
+  | EnglishUnitedStates
+  | EnglishZimbabwe
+  | Estonian
+  | Faeroese
+  | Finnish
+  | French
+  | FrenchBelgium
+  | FrenchCanada
+  | FrenchFrance
+  | FrenchLuxembourg
+  | FrenchMonaco
+  | FrenchSwitzerland
+  | Galician
+  | Gaelic
+  | German
+  | GermanAustria
+  | GermanGermany
+  | GermanLiechtenstein
+  | GermanLuxembourg
+  | GermanSwitzerland
+  | Greek
+  | Hawaiian
+  | Hungarian
+  | Icelandic
+  | Indonesian
+  | Irish
+  | Italian
+  | ItalianItaly
+  | ItalianSwitzerland
+  | Japanese
+  | Korean
+  | Macedonian
+  | Norwegian
+  | Polish
+  | Portuguese
+  | PortugueseBrazil
+  | PortugeesePortugal
+  | Romanian
+  | RomanianMoldova
+  | RomanianRomania
   | Russian
+  | RussianMoldova
+  | RussianRussia
+  | Serbian
+  | Slovak
+  | Slovenian
   | Spanish
+  | SpanishArgentina
+  | SpanishBolivia
+  | SpanishChile
+  | SpanishColombia
+  | SpanishCostaRica
+  | SpanishDominicanRepublic
+  | SpanishEcuador
+  | SpanishElSalvador
+  | SpanishGuatemala
+  | SpanishHonduras
+  | SpanishMexico
+  | SpanishNicaragua
+  | SpanishPanama
+  | SpanishParaguay
+  | SpanishPeru
+  | SpanishPuertoRico
+  | SpanishSpain
+  | SpanishUruguay
+  | SpanishVenezuela
+  | Swedish
+  | SwedishFinland
+  | SwedishSweden
+  | Turkish
+  | Ukrainian
   deriving (Eq, Ord, Bounded, Enum, Show, Read)
 
 -- | Render a 'RSSFeed' as a lazy 'BL.ByteString'.
@@ -120,16 +203,100 @@ renderFeed = undefined
 -- | Get identifier for a 'Language'.
 
 getLangId :: Language -> Text
-getLangId Africans           = "af"
-getLangId Albanian           = "sq"
-getLangId Basque             = "eu"
-getLangId Bulgarian          = "bg"
-getLangId Catalan            = "ca"
-getLangId ChineseSimplified  = "zh-cn"
-getLangId ChineseTraditional = "zh-tw"
-getLangId Croatian           = "hr"
-getLangId Czech              = "cs"
-
-getLangId English            = "en"
-getLangId Russian            = "ru"
-getLangId Spanish            = "es"
+getLangId = \case
+  Africans           -> "af"
+  Albanian           -> "sq"
+  Basque             -> "eu"
+  Belarusian         -> "be"
+  Bulgarian          -> "bg"
+  Catalan            -> "ca"
+  ChineseSimplified  -> "zh-cn"
+  ChineseTraditional -> "zh-tw"
+  Croatian           -> "hr"
+  Czech              -> "cs"
+  Danish             -> "da"
+  Dutch              -> "nl"
+  DutchBelgium       -> "nl-be"
+  DutchNetherlands   -> "nl-nl"
+  English            -> "en"
+  EnglishAustralia   -> "en-au"
+  EnglishBelize      -> "en-bz"
+  EnglishCanada      -> "en-ca"
+  EnglishIreland     -> "en-ie"
+  EnglishJamaica     -> "en-jm"
+  EnglishNewZealand  -> "en-nz"
+  EnglishPhillipines -> "en-ph"
+  EnglishSouthAfrica -> "en-za"
+  EnglishTrinidad    -> "en-tt"
+  EnglishUnitedKingdom -> "en-gb"
+  EnglishUnitedStates -> "en-us"
+  EnglishZimbabwe    -> "en-zw"
+  Estonian           -> "et"
+  Faeroese           -> "fo"
+  Finnish            -> "fi"
+  French             -> "fr"
+  FrenchBelgium      -> "fr-be"
+  FrenchCanada       -> "fr-ca"
+  FrenchFrance       -> "fr-fr"
+  FrenchLuxembourg   -> "fr-lu"
+  FrenchMonaco       -> "fr-mc"
+  FrenchSwitzerland  -> "fr-ch"
+  Galician           -> "gl"
+  Gaelic             -> "gd"
+  German             -> "de"
+  GermanAustria      -> "de-at"
+  GermanGermany      -> "de-de"
+  GermanLiechtenstein -> "de-li"
+  GermanLuxembourg   -> "de-lu"
+  GermanSwitzerland  -> "de-ch"
+  Greek              -> "el"
+  Hawaiian           -> "haw"
+  Hungarian          -> "hu"
+  Icelandic          -> "is"
+  Indonesian         -> "in"
+  Irish              -> "ga"
+  Italian            -> "it"
+  ItalianItaly       -> "it-it"
+  ItalianSwitzerland -> "it-ch"
+  Japanese           -> "ja"
+  Korean             -> "ko"
+  Macedonian         -> "mk"
+  Norwegian          -> "no"
+  Polish             -> "pl"
+  Portuguese         -> "pt"
+  PortugueseBrazil   -> "pt-br"
+  PortugeesePortugal -> "pt-pt"
+  Romanian           -> "ro"
+  RomanianMoldova    -> "ro-mo"
+  RomanianRomania    -> "ro-ro"
+  Russian            -> "ru"
+  RussianMoldova     -> "ru-mo"
+  RussianRussia      -> "ru-ru"
+  Serbian            -> "sr"
+  Slovak             -> "sk"
+  Slovenian          -> "sl"
+  Spanish            -> "es"
+  SpanishArgentina   -> "es-ar"
+  SpanishBolivia     -> "es-bo"
+  SpanishChile       -> "es-cl"
+  SpanishColombia    -> "es-co"
+  SpanishCostaRica   -> "es-cr"
+  SpanishDominicanRepublic -> "es-do"
+  SpanishEcuador     -> "es-ec"
+  SpanishElSalvador  -> "es-sv"
+  SpanishGuatemala   -> "es-gt"
+  SpanishHonduras    -> "es-hn"
+  SpanishMexico      -> "es-mx"
+  SpanishNicaragua   -> "es-ni"
+  SpanishPanama      -> "es-pa"
+  SpanishParaguay    -> "es-py"
+  SpanishPeru        -> "es-pe"
+  SpanishPuertoRico  -> "es-pr"
+  SpanishSpain       -> "es-es"
+  SpanishUruguay     -> "es-uy"
+  SpanishVenezuela   -> "es-ve"
+  Swedish            -> "sv"
+  SwedishFinland     -> "sv-fi"
+  SwedishSweden      -> "sv-se"
+  Turkish            -> "tr"
+  Ukrainian          -> "uk"
