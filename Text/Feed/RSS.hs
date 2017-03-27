ca8af7f8c03d3f228ad49ce2b19071e627dbfa52
mrkkrp <markkarpov@opmbx.org>
2017-03-28 00:03:44 +0300

    Finish definition of ‘Feed’, some renaming

--- Text/Feed/RSS.hs
+++ Text/Feed/RSS.hs
@@ -8,96 +8,111 @@
 -- Portability :  portable
 --
 -- Everything you need for RSS feed generation.
+--
+-- This module is intended to be imported qualified:
+--
+-- > import qualified Text.Feed.RSS
 
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 
 module Text.Feed.RSS
-  ( RSSFeed (..)
-  , RSSItem (..)
-  , renderRSSFeed )
+  ( -- * Types
+    Feed (..)
+  , Item (..)
+  , Image (..)
+    -- * Feed rendering
+  , renderFeed )
 where
 
-import Data.Time.Clock (UTCTime)
 import Data.Text (Text)
+import Data.Time.Clock (UTCTime)
 import qualified Data.ByteString.Lazy as BL
 
 -- | A representation of a RSS feed.
 --
 -- See also: <http://www.rssboard.org/rss-specification>.
 
-data RSSFeed = RSSFeed
-  { rssFeedTitle :: !Text
+data Feed = Feed
+  { feedTitle :: !Text
     -- ^ The name of the channel
-  , rssFeedLink :: !URL
+  , feedLink :: !Text
     -- ^ The URL to the HTML website
-  , rssFeedDescription :: !Text
+  , feedDescription :: !Text
     -- ^ Phrase or sentence describing the channel
-  , rssFeedLanguage :: !(Maybe Language)
+  , feedLanguage :: !(Maybe Language)
     -- ^ The language the channel is written in
-  , rssCopyright :: !(Maybe Text)
+  , feedCopyright :: !(Maybe Text)
     -- ^ Copyright notice for content in the channel
-  , rssManagingEditor :: !(Maybe Email)
-    -- ^ Email for responsible of editorial content
-  , rssWebMaster :: !(Maybe Email)
-    -- ^ Email for resposible of technical issues
-  , rssPubDate :: !(Maybe UTCTime)
+  , feedManagingEditor :: !(Maybe Text)
+    -- ^ Email address for person responsible of editorial content
+  , feedWebMaster :: !(Maybe Text)
+    -- ^ Email address for person responsible of technical issues relating
+    -- to the channel
+  , feedPubDate :: !(Maybe UTCTime)
     -- ^ Publication date for the content
-  , rssLastBuildDate :: !(Maybe UTCTime)
+  , feedLastBuildDate :: !(Maybe UTCTime)
     -- ^ Last time the content changed
-  , rssCategory :: !(Maybe [Text])
-    -- ^ One or more for categories that the channel belongs to
-  , rssGenerator :: !(Maybe Text)
-    -- ^ A string indicating the program used to generate channel
-  , rssDocs :: !(Maybe URL)
+  , feedCategory :: ![Text]
+    -- ^ One or more categories that the channel belongs to
+  , feedDocs :: !(Maybe Text)
     -- ^ A URL pointing to the documentation for the format used
-  , rssCloud :: !(Maybe Cloud)
-    -- ^ Allows processes to register with a cloud
-  , rssTTL :: !(Maybe Integer)
-    -- ^ Number of mintes for how long a channel can be cached
-  , rssImage :: !(Maybe )
-  , rssFeedItems :: ![RSSItem]
+  , feedTimeToLive :: !(Maybe Int)
+    -- ^ Number of minutes that indicates how long a channel can be cached
+    -- before refreshing from the source
+  , feedImage :: !(Maybe Image)
+    -- ^ Image to be displayed with channel
+  , feedItems :: ![Item]
     -- ^ Collection of feed items
   } deriving (Eq, Ord, Show, Read)
 
--- |
+-- | An RSS item
 
-data RSSItem = RSSItem
+data Item = Item
   {
   } deriving (Eq, Ord, Show, Read)
 
-type Email = Text
-type URL   = Text
-
-data Cloud = Cloud
-  { cloudDomain   :: !Text
-  , cloudPort     :: !Text
-  , cloudPath     :: !Text
-  , cloudProtocol :: !Text
-  , cloudRegisterProcedure :: !Text
-  }
+-- | Information about image to be displayed with channel.
 
 data Image = Image
-  { imageUrl :: !URL
+  { imageUrl   :: !Text
+    -- ^ The URL of a GIF, JPEG or PNG image that represents the channel
   , imageTitle :: !Text
-  , imageLink  :: !URL
-  , image
-  }
+    -- ^ Description of the image, will be used as @alt@ attribute when the
+    -- image is rendered in HTML
+  , imageLink  :: !Text
+    -- ^ URL of the site, when the image
+  , imageWidth :: !(Maybe Int)
+    -- ^ Optional width: maximum value is 144, default value is 88
+  , imageHeigh :: !(Maybe Int)
+    -- ^ Optional height: maximum height is 400, default value is 31
+  } deriving (Eq, Ord, Show, Read)
 
 -- | Enumeration of languages.
 --
 -- See also: <http://www.rssboard.org/rss-language-codes>.
 
 data Language
-  = English
+  = Africans
+  | Albanian
+  | Basque
+  | Belarusian
+  | Bulgarian
+  | Catalan
+  | ChineseSimplified
+  | ChineseTraditional
+  | Croatian
+  | Czech
+
+  | English
   | Russian
   | Spanish
   deriving (Eq, Ord, Bounded, Enum, Show, Read)
 
 -- | Render a 'RSSFeed' as a lazy 'BL.ByteString'.
 
-renderRSSFeed :: RSSFeed -> BL.ByteString
-renderRSSFeed = undefined
+renderFeed :: Feed -> BL.ByteString
+renderFeed = undefined
 
 ----------------------------------------------------------------------------
 -- Helpers
@@ -105,6 +120,16 @@ renderRSSFeed = undefined
 -- | Get identifier for a 'Language'.
 
 getLangId :: Language -> Text
-getLangId English = "en"
-getLangId Russian = "ru"
-getLangId Spanish = "es"
+getLangId Africans           = "af"
+getLangId Albanian           = "sq"
+getLangId Basque             = "eu"
+getLangId Bulgarian          = "bg"
+getLangId Catalan            = "ca"
+getLangId ChineseSimplified  = "zh-cn"
+getLangId ChineseTraditional = "zh-tw"
+getLangId Croatian           = "hr"
+getLangId Czech              = "cs"
+
+getLangId English            = "en"
+getLangId Russian            = "ru"
+getLangId Spanish            = "es"
